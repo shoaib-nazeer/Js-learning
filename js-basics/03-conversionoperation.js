1cc66279642bed06b1dfad4e4f01259211eed3cb
Shoaib Nazeer <shoaibnazeer3922@gmail.com>
2026-05-15 14:16:35 +0000

    Conersion of datatypes.

---
+++ js-basics/03-conversionoperation.js
@@ -0,0 +1,38 @@
+// String to Number conversion
+
+
+// let age = "33";
+
+
+// let convertedAge = Number(age);
+
+// console.log(convertedAge);
+// console.log(typeof (convertedAge));
+
+// ..Second Example..
+
+// Number to String conversion
+
+// let score = 33;
+
+// console.log(score);
+// console.log(typeof (score));
+
+
+// let convertedScore = String(score);
+// console.log(convertedScore)
+// console.log(typeof (convertedScore))
+
+
+// ..Third Example..
+
+let age = true
+
+// console.log(age);
+
+
+let convertedAge = Number (age)
+
+console.log(convertedAge);
+console.log(typeof (convertedAge));
+
