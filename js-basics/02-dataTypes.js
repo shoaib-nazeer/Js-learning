943ac9d065b5b069055e49b16eccd842cae815b7
Shoaib Nazeer <shoaibnazeer3922@gmail.com>
2026-05-15 12:36:01 +0000

    dataTypes of js

---
+++ js-basics/02-dataTypes.js
@@ -0,0 +1,11 @@
+// number = 2 ^ 53
+// bigint = for large numbers
+// string = "assembly of characters"
+// boolean = true or false
+// null = stand alone value (type = object)
+// undefined = kindda place holder for a value which is not defined yet (type = undefined)
+// symbol = uniquness
+
+
+// # "use strict";   treat all code as newer version of JS
+
